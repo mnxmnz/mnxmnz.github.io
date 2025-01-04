889f941666c64a09bff67dd99292d509436fdb12
mnxmnz <minzidev@gmail.com>
2025-01-04 17:25:19 +0900

    feat: add article link to navigation

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,6 +7,10 @@ export const NAV_LINK_ITEMS = [
     title: 'Category',
     url: '/category',
   },
+  {
+    title: 'Article',
+    url: '/article',
+  },
 ];
 
 export const COPYRIGHT = `© 2021-${new Date().getFullYear()} 김민지 All Rights Reserved.`;
