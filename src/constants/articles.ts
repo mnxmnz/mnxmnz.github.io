667751c242c610003f15062178f6e4a137657a18
mnxmnz <minzidev@gmail.com>
2025-01-04 18:08:59 +0900

    feat: add new articles to the articles constant

--- src/constants/articles.ts
+++ src/constants/articles.ts
@@ -5,4 +5,23 @@ export const articles = [
     review: '나는 왜 "개발자"로 일하고 있을까?',
     publishedAt: '2024-12-23',
   },
+  {
+    url: 'https://steady-study.super.site/frontend-engineer-career-roadmap',
+    title: '프론트엔드 엔지니어 커리어 로드맵: 주니어를 위한 3가지 전문성 트랙',
+    review: '항상 기본에 충실하고 큰 임팩트를 낼 수 있도록 오너십을 가지자',
+    publishedAt: '2022-12-28',
+  },
+  {
+    url: 'https://if.kakao.com/session/39',
+    title: '접근성 작업을 도와주는 ESLint plugin 개발기',
+    review:
+      '평소에 느끼는 불편함에 익숙해지지 않고 새로운 해결 방법을 찾아서 효율성 개선하기',
+    publishedAt: '2024-10-23',
+  },
+  {
+    url: 'https://vercel.com/blog/how-core-web-vitals-affect-seo#how-google-ranks-based-on-page-speed',
+    title: 'How Core Web Vitals affect SEO',
+    review: '',
+    publishedAt: '2024-01-19',
+  },
 ];
