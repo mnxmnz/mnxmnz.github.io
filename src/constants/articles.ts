21343c6486393f00c2c0831b8f394c02a33d5168
mnxmnz <minzidev@gmail.com>
2025-01-04 17:25:12 +0900

    feat: add articles to constants

---
+++ src/constants/articles.ts
@@ -0,0 +1,8 @@
+export const articles = [
+  {
+    url: 'https://evan-moon.github.io/2024/12/23/the-illusion-of-a-right-answer',
+    title: '좋은 코드란 무엇일까? - 가독성이란 허상에 대하여',
+    review: '나는 왜 "개발자"로 일하고 있을까?',
+    publishedAt: '2024-12-23',
+  },
+];
