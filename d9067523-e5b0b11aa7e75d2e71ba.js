02b2978d33266e941ee7cd5e632a9d1c0cfafd5b
mnxmnz <mnxmnz@users.noreply.github.com>
2026-01-11 17:50:57 +0000

    deploy: 7dbd6138c7fe3484a78d524e5715d37f311061e1

---
+++ d9067523-e5b0b11aa7e75d2e71ba.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdevlog=self.webpackChunkdevlog||[]).push([[945],{712:function(t,e,n){n.d(e,{nP5:function(){return l}});var c=n(2225);function l(t){return(0,c.k5)({tag:"svg",attr:{role:"img",viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=d9067523-e5b0b11aa7e75d2e71ba.js.map
